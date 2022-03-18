2c1072c76e147e38ebf8b1d6a9cafbd264e4dd23
Michael <Michael@DESKTOP-JGFB3CN>
2022-03-18 17:47:53 +0100

    Add project files.

---
+++ CurrencyConverter/IService1.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace CurrencyConverter
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        ListOfCurrencies CurrenciesList();
+
+        [OperationContract]
+        CurrencyItem CurrencyPerCode(string currencyCode);
+
+    }
+
+    [DataContract]
+    public class ListOfCurrencies
+    {
+        private List<CurrencyItem> currencyItems;
+
+        [DataMember]
+        public List<CurrencyItem> CurrencyItems
+        {
+            get
+            {
+                if (currencyItems == null)
+                {
+                    currencyItems = new List<CurrencyItem>();
+                }
+                return currencyItems;
+            }
+        }
+    }
+
+
+
+    [DataContract]
+    public class CurrencyItem
+    {
+        private string symbol;
+        private double rate;
+
+        [DataMember]
+        public string Symbol
+        {
+            get { return symbol; }
+            set { symbol = value; }
+        }
+
+        [DataMember]
+        public double Rate
+        {
+            get { return rate; }
+            set { rate = value; }
+        }
+
+
+    }
+
+
+}
---
+++ CurrencyConverter/Service1.svc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Xml;
+
+namespace CurrencyConverter
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public ListOfCurrencies CurrenciesList()
+        {
+            return Data();
+        }
+
+        public CurrencyItem CurrencyPerCode(string currencyCode)
+        {
+            ListOfCurrencies listOfCurrencies = Data();
+            CurrencyItem currencyItem = new CurrencyItem();
+
+            for (int i = 0; i < listOfCurrencies.CurrencyItems.Count; i++)
+            {
+                if (listOfCurrencies.CurrencyItems[i].Symbol == currencyCode)
+                {
+                    currencyItem = listOfCurrencies.CurrencyItems[i];
+                }
+            }
+            return currencyItem;
+        }
+
+        public static ListOfCurrencies Data()
+        {
+            ListOfCurrencies listOfCurrencies = new ListOfCurrencies();
+            
+            XmlReader xmlReader = XmlReader.Create("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+            while(xmlReader.Read())
+            {
+                if((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube"))
+                {
+                    if (xmlReader.HasAttributes)
+                        if(xmlReader.GetAttribute("currency") != null && xmlReader.GetAttribute("rate") != null)
+                        {
+                            CurrencyItem currencyItem = new CurrencyItem();
+                            currencyItem.Symbol = xmlReader.GetAttribute("currency");
+                            currencyItem.Rate = Convert.ToDouble(xmlReader.GetAttribute("rate"),System.Globalization.CultureInfo.InvariantCulture);
+                            listOfCurrencies.CurrencyItems.Add(currencyItem);
+                        }     
+                }
+            }
+            return listOfCurrencies;
+        }
+    }
+
+}
